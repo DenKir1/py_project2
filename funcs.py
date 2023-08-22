ab91dab56ff87e3ab61562d3cc4249d2df0c39dd
Denis Kirdyankin <kird.den@rambler.ru>
2023-08-22 17:14:06 +0300

    Sum

---
+++ funcs.py
@@ -0,0 +1,11 @@
+
+def summ_two(a, b):
+    if type(a) in (int, float) and type(b) in (int, float):
+        return a + b
+
+
+def minus_two(a, b):
+    if type(a) in (int, float) and type(b) in (int, float):
+        return a - b
+
+
